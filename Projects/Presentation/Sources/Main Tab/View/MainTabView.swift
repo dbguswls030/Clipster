6b684b8b3fffda694449c1087355c23cb446d751
dbguswls030 <dbguswls030@gmail.com>
2024-11-18 23:54:21 +0900

    feat: 리팩토링 및 viewModel 생성 후 로직 분리

--- Projects/Presentation/Sources/Main Tab/View/MainTabView.swift
+++ Projects/Presentation/Sources/Main Tab/View/MainTabView.swift
@@ -8,17 +8,15 @@
 import SwiftUI
 
 public struct MainTabView: View {
+    
+    @StateObject private var viewModel = MainTabViewModel()
     @Environment(\.scenePhase) private var scenePhase
-    @State private var selection: TabCase = .clips
-    @State private var hasPasteBoard: Bool = false
-    @State private var pastedURL: URL? = nil
-    @State private var toast: ToastModel? = nil
-    @State private var workItem: DispatchWorkItem?
+    
     public init() {}
     
     public var body: some View {
         
-        TabView(selection: $selection){
+        TabView(selection: $viewModel.selection){
             MyClipView()
                 .tabItem {
                     Label("clips", systemImage: "star")
@@ -30,39 +28,20 @@ public struct MainTabView: View {
                 }
                 .tag(TabCase.myPage)
         }
-        .onChange(of: scenePhase) { phase in
-            if phase == .active, UIPasteboard.general.hasURLs{
-                hasPasteBoard = true
-            }else if phase == .background{
-                hasPasteBoard = false
-            }
-        }
-        .onChange(of: hasPasteBoard) {
-            if $0{
-                if pastedURL != UIPasteboard.general.url{
-                    pastedURL = UIPasteboard.general.url
-                }
-            }
-        }
-        .onChange(of: pastedURL) { newPastedURL in
-            print(newPastedURL)
-            if let newURL = newPastedURL{
-                toast = ToastModel(url: newURL)
-            }
-        }
+        .onChange(of: scenePhase, perform: viewModel.handleScenePhaseChange)
+        .onChange(of: viewModel.hasPasteBoard, perform: viewModel.pasteURL)
+        .onChange(of: viewModel.pastedURL, perform: viewModel.makeURL)
         .overlay {
             ZStack(){
                 mainToastView()
-            }.animation(.spring, value: toast)
+            }.animation(.spring, value: viewModel.toast)
                 .padding(.horizontal)
         }
-        .onChange(of: toast) { newToast in
-            showToast()
-        }
+        .onChange(of: viewModel.toast, perform: { _ in viewModel.showToast() })
     }
     
     @ViewBuilder func mainToastView() -> some View {
-        if let toast = toast {
+        if let toast = viewModel.toast {
             VStack {
                 Spacer()
                 ToastView(url: toast.url)
@@ -70,32 +49,6 @@ public struct MainTabView: View {
             }
             .transition(.move(edge: .bottom))
         }
-    }
-    
-    private func showToast() {
-        guard let toast = toast else { return }
-        
-        UIImpactFeedbackGenerator(style: .light).impactOccurred()
-        
-        if toast.duration > 0 {
-            workItem?.cancel()
-            
-            let task = DispatchWorkItem {
-                dismissToast()
-            }
-            
-            workItem = task
-            DispatchQueue.main.asyncAfter(deadline: .now() + toast.duration, execute: task)
-        }
-    }
-    
-    private func dismissToast() {
-        withAnimation {
-            toast = nil
-        }
-        
-        workItem?.cancel()
-        workItem = nil
     }
 }
 
---
+++ Projects/Presentation/Sources/Main Tab/ViewModel/MainTabViewModel.swift
@@ -0,0 +1,66 @@
+//
+//  MainTabViewModel.swift
+//  Presentation
+//
+//  Created by 유현진 on 11/18/24.
+//
+
+import SwiftUI
+
+final class MainTabViewModel: ObservableObject{
+    
+    @Published var selection: TabCase = .clips
+    @Published var hasPasteBoard: Bool = false
+    @Published var pastedURL: URL? = nil
+    @Published var toast: ToastModel? = nil
+    @Published var workItem: DispatchWorkItem?
+    
+    
+    func handleScenePhaseChange(_ phase: ScenePhase){
+        if phase == .active{
+            hasPasteBoard = true
+        }else if phase == .background{
+            hasPasteBoard = false
+        }
+    }
+    
+    func pasteURL(_ hasPasteBoard: Bool){
+        if hasPasteBoard{
+            if pastedURL != UIPasteboard.general.url{
+                pastedURL = UIPasteboard.general.url
+            }
+        }
+    }
+    
+    func makeURL(_ url: URL?){
+        if let newURL = url{
+            toast = ToastModel(url: newURL)
+        }
+    }
+    
+    func showToast() {
+        guard let toast = toast else { return }
+        
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+        
+        if toast.duration > 0 {
+            workItem?.cancel()
+            
+            let task = DispatchWorkItem { [weak self] in
+                self?.dismissToast()
+            }
+            
+            workItem = task
+            DispatchQueue.main.asyncAfter(deadline: .now() + toast.duration, execute: task)
+        }
+    }
+    
+    func dismissToast() {
+        withAnimation {
+            toast = nil
+        }
+        
+        workItem?.cancel()
+        workItem = nil
+    }
+}

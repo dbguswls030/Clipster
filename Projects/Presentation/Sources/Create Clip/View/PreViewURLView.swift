41f81cdf9ede3776adb890cbbd83d72e696992cf
dbguswls030 <dbguswls030@gmail.com>
2024-11-25 21:32:05 +0900

    feat: HTML 메타데이터 프리뷰 생성

---
+++ Projects/Presentation/Sources/Create Clip/View/PreViewURLView.swift
@@ -0,0 +1,18 @@
+//
+//  PreViewURLView.swift
+//  Presentation
+//
+//  Created by 유현진 on 11/25/24.
+//
+
+import SwiftUI
+
+struct PreViewURLView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreViewURLView()
+}

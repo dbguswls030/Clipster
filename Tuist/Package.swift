72b382fe7958c7d1562b714acf8edf1eeb88b3a5
유현진 <dbguswls030@gmail.com>
2025-01-03 12:25:58 +0900

    config: moya 모듈 삭제

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -10,8 +10,6 @@ let packageSettings = PackageSettings(
     // productTypes: ["Alamofire": .framework,]
     productTypes: [
         "SwiftSoup": .framework,
-        "Moya" : .framework,
-        "CombineMoya" : .framework,
     ]
 )
 #endif
@@ -23,7 +21,6 @@ let package = Package(
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", .upToNextMajor(from: "2.7.5")),
         .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "11.5.0")),
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.3"))
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
     ]
 )
